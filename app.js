574096d02a2848e40c34c49c820b7fdbfba4b466
Sebastian-Simek <sebastiansimek09@gmail.com>
2022-04-26 12:19:28 -0700

    Started work on function

--- app.js
+++ app.js
@@ -37,8 +37,16 @@ boulderButton.addEventListener('click', () => {
 
 function handleGuess(correctSpot, userGuess) {
     // reset the styles
-    // then increment the guesses
-    // then grab the appropriate container element for the correct guess from the DOM
+    treeContainer.classList.remove('face');
+    boulderContainer.classList.remove('face');
+    shedContainer.classList.remove('face');
+        // then grab the appropriate container element for the correct guess from the DOM    
+    if (userGuess === correctSpot) {
+        treeContainer.classList.add('face');
+        correctGuesses++;
+        totalGuesses++;
+    } else 
+    
     // then add the face class to that element so that the face shows up
     // then if the user guess is correct, increment the correct guesses
     // update the DOM to show this change to the user (including the losses, not tracked directly in state)
